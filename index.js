492bb1b3ed9a8802eb5eed19e26ae349b4eb51c1
Fabian <fabiancardozo99@gmail.com>
2026-03-15 22:57:41 -0300

    Implement function for creating notes and generate id's

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import * as notes from "./notes.js";
+
+const action = process.argv[2];
+const argsOfAction = process.argv.slice(3);
+
+const actions = new Map([
+  ["add", notes.createNote],
+  ["list", notes.listNotes],
+  ["delete", notes.deleteNote],
+  ["update", notes.updateNote],
+]);
+
+const command = actions.get(action);
+
+command(...argsOfAction);
--- notes.js
+++ notes.js
@@ -15,3 +15,40 @@ export async function saveNotes(udpatedNotes) {
   const jsonData = JSON.stringify(udpatedNotes, null, 2);
   await fs.writeFile(NOTES_FILE, jsonData);
 }
+
+function generateId(notes) {
+  let newId = 0;
+
+  for (const note of notes) {
+    if (note.id > newId) {
+      newId = note.id;
+    }
+  }
+
+  return newId + 1;
+}
+
+export async function createNote(text) {
+  const notes = await loadNotes();
+
+  const newNote = {
+    id: generateId(notes),
+    text,
+  };
+
+  notes.push(newNote);
+
+  await saveNotes(notes);
+}
+
+export function listNotes() {
+  console.log("Hello list");
+}
+
+export function deleteNote(id) {
+  console.log(id);
+}
+
+export function updateNote(id, note) {
+  console.log(`${id}) ${note}`);
+}
